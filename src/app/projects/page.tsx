6cab753cbfa057b1ff6dbfcd7c8caa24ac5a0f80
Dima Rui <drankendmitrij@gmail.com>
2026-01-28 22:04:43 +0200

    updated: projects page ui;

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,27 @@
 import { Page } from '@/shared/ui/Page';
+import s from './page.module.scss';
+import { Button } from '@/shared/ui/Button';
+import { Plus } from 'lucide-react';
+import { Input } from '@/shared/ui/Input';
 
 export default function Projects() {
-	return <Page>Projects</Page>;
+	return (
+		<Page className={s.projects}>
+			<div className={s.top}>
+				<div className={s.left}>
+					<h1 className={s.title}>Все проекты</h1>
+					<p className={s.description}>6 активных проектов</p>
+				</div>
+				<Button className={s.createProject}>
+					<Plus size={18} />
+					Создать проект
+				</Button>
+			</div>
+			<Input
+				placeholder='Поиск проектов...'
+				wrapperClass={s.inputWrapper}
+				isSearch
+			/>
+		</Page>
+	);
 }
